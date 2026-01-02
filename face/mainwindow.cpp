367ab57680ddba17f6ca33cc7ed680517c5a598a
WANG,SHI-HAO <41343107@nfu.edu.tw>
2026-01-02 20:16:11 +0800

    Add files via upload

---
+++ face/mainwindow.cpp
@@ -0,0 +1,87 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+
+    cap.open(0);
+
+    timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, this, &MainWindow::updateFrame);
+    timer->start(60);
+    doorTimer = new QTimer(this);
+    doorTimer->setSingleShot(true);
+
+    connect(doorTimer, &QTimer::timeout, this, [=]() {
+        doorOpen = false;
+        ui->label_status->setText("Door Locked");
+        ui->label_status->setStyleSheet("color: red; font-weight: bold;");
+    });
+
+
+}
+void MainWindow::updateFrame()
+{
+    cv::Mat frame;
+    cap >> frame;
+    if (frame.empty()) return;
+
+    cv::Mat gray;
+    cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
+
+    static cv::CascadeClassifier face_cascade;
+    static bool loaded = false;
+    if(!loaded) {
+        if(!face_cascade.load("haarcascade_frontalface_default.xml")) {
+            qDebug() << "Error loading face cascade";
+            return;
+        }
+        loaded = true;
+    }
+
+    std::vector<cv::Rect> faces;
+    face_cascade.detectMultiScale(gray, faces, 1.1, 3, 0, cv::Size(30, 30));
+    if (!faces.empty() && !facePresent) {
+        facePresent = true;
+
+        if (!doorOpen) {
+            doorOpen = true;
+            ui->label_status->setText("有人\nDoor Open");
+            ui->label_status->setStyleSheet("color: green; font-weight: bold;");
+            doorTimer->start(3000);
+        }
+    }
+    else if (faces.empty()) {
+        facePresent = false;
+    }
+
+    for (size_t i = 0; i < faces.size(); i++) {
+        cv::rectangle(frame, faces[i], cv::Scalar(0, 255, 0), 2);
+    }
+
+    cv::cvtColor(frame, frame, cv::COLOR_BGR2RGB);
+    QImage img(
+        frame.data,
+        frame.cols,
+        frame.rows,
+        frame.step,
+        QImage::Format_RGB888
+        );
+
+    ui->label_camera->setPixmap(
+        QPixmap::fromImage(img).scaled(
+            ui->label_camera->size(),
+            Qt::KeepAspectRatio
+            )
+        );
+}
+
+MainWindow::~MainWindow()
+{
+    cap.release();
+    delete ui;
+}
---
+++ face/mainwindow.h
@@ -0,0 +1,27 @@
+#include <QMainWindow>
+#include <QTimer>
+#include <opencv2/opencv.hpp>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void updateFrame();   // 更新影像
+
+private:
+    Ui::MainWindow *ui;
+    QTimer *timer;
+    cv::VideoCapture cap; // 攝影機
+    bool doorOpen = false;
+    QTimer *doorTimer;
+    bool facePresent = false;
+};
